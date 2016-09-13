656cbdfeefa3a42b40977e8debdb4d1d0d77cd6d
Wojciech "Zarazek" Wiśniewski <balagane@op.pl>
2016-09-13 13:05:25 +0100

    Switched from csv-conduit to cassava-conduit for better error handling.

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,33 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module Main where
 import Data.Conduit (ConduitM, (=$=), transPipe, runConduit)
 import Data.Conduit.List (sourceList)
 import Conduit
 import Data.Conduit.BZlib (bunzip2)
 import Codec.Archive.Tar (conduitEntry, FormatError)
-import Codec.Archive.Tar.Entry (entryPath, entryContent, EntryContent(..), FileSize)
-import Data.CSV.Conduit (intoCSV, CSVSettings(..), Row)
+import Codec.Archive.Tar.Entry (entryPath, entryContent, EntryContent(..))
+import Data.Csv (DecodeOptions(..), HasHeader(..))
+import Data.Csv.Conduit (CsvStreamHaltParseError, CsvStreamRecordParseError, fromCsvStreamError)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Trie as T
 import System.Environment (getArgs)
 import Control.Monad.Trans.Resource (ResourceT, transResourceT, runResourceT)
 import Control.Monad.Trans.Either (EitherT, bimapEitherT, runEitherT)
-import Control.Monad.Catch (throwM)
-import Control.Exception (Exception, displayException, AssertionFailed(..))
 import Control.DeepSeq (force)
 import Data.Foldable (for_)
 import Text.Printf (printf)
 import Data.Char (ord)
-
+import Data.Either (isRight)
 
 data PipelineError = TarError FormatError
-  deriving Show
+                   | CsvError FilePath CsvStreamHaltParseError
+ deriving Show
 
 type M = ResourceT (EitherT PipelineError IO)
 
 transformError f = transResourceT (bimapEitherT f id)
 
-tarBz2Pipeline :: ConduitM BS.ByteString (FilePath, FileSize, LBS.ByteString) M ()
+tarBz2Pipeline :: ConduitM BS.ByteString (FilePath, LBS.ByteString) M ()
 tarBz2Pipeline = bunzip2 =$=
                  transPipe (transformError TarError) conduitEntry =$=
                  filterC (isNormalFile . entryContent) =$=
@@ -37,34 +35,36 @@ tarBz2Pipeline = bunzip2 =$=
   where isNormalFile (NormalFile _ _) = True
         isNormalFile  Directory       = False
         isNormalFile  _               = error "unexpected entry type"
-        retrieveContents e = (entryPath e, fileSize c, fileContents c)
-          where fileSize (NormalFile _ s) = s
-                fileSize _                = error "fileSize: not an ordinary file"
-                fileContents (NormalFile c _) = c
+        retrieveContents e = (entryPath e, fileContents (entryContent e))
+          where fileContents (NormalFile c _) = c
                 fileContents _                = error "fileContents: not an ordinary file"
-                c = entryContent e
 
-csvPipeline :: ConduitM BS.ByteString (Row BS.ByteString) M ()
-csvPipeline = intoCSV options
-  where options = CSVSettings { csvSep = '?', csvQuoteChar = Nothing }
+csvPipeline :: FilePath -> ConduitM BS.ByteString (Either CsvStreamRecordParseError [BS.ByteString]) M ()
+csvPipeline fileName = fromCsvStreamError options NoHeader (CsvError fileName)
+  where options = DecodeOptions { decDelimiter = fromIntegral (ord '?') }
 
 pipeline :: FilePath -> ConduitM i o M [T.Trie ()]
 pipeline fileName = sourceFile fileName =$=
                     tarBz2Pipeline =$=
-                    mapMC (\t -> liftIO (putStrLn (first t)) >> return (third t)) =$=
-                    -- (throwM (AssertionFailed "dupa") >> concatMapMC runCsvPipeline) =$=
+                    iterMC (liftIO . putStrLn . fst) =$=
                     concatMapMC runCsvPipeline =$=
-
-                    foldlC (force . zipWith setInsert) emptySets 
-  where first (x, _, _) = x
-        third (_, _, x) = x
-        runCsvPipeline lbs = runConduit pipe
-          where pipe = sourceLazy lbs =$= csvPipeline =$= sinkList
+                    foldlC (force . zipWith setInsert) emptySets
+  where runCsvPipeline (name, lbs) = runConduit pipe
+          where pipe = sourceLazy lbs =$=
+                       csvPipeline name =$=
+                       iterMC printError =$=
+                       filterC isRight =$=
+                       mapC fromRight =$=
+                       sinkList
+                printError x = case x of
+                  Left e  -> liftIO $ putStrLn (name ++ ": " ++ show e)
+                  Right _ -> return ()
+        fromRight (Right x) = x
         setInsert s e = T.insert e () s
         emptySets = repeat T.empty
 
 processResults res = for_ (zip [(1 :: Int)..] res) $ \(i, s) -> do
- putStrLn (show i)
+ putStrLn (printf "%03d -> %d" i (T.size s))
  let pipeline = sourceList (T.keys s) =$=
                 mapC (flip BS.snoc (fromIntegral (ord '\n'))) =$=
                 sinkFile (printf "%03d.txt" i)
